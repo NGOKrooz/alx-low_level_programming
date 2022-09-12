6e5fbacd2bd7ee696349be07e552bd8940535701
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-12 06:01:13 -0700

    Task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+#include <stdint.h>
 /**
  * main - print a fuction
  *
  * Description: using the main function
- * this program prints "C program that prints the size of various types on the computer"
  * Return: 0
  */
 int main(void)
@@ -13,11 +13,10 @@ int i;
 long li;
 long long lli;
 float f;
-
-printf("size of char: %ld byte(s)\n", sizeof(c));
-printf("size of int: %ld byte(s)\n", sizeof(i));
-printf("size of long int: %ld byte(s)\n", sizeof(li));
-printf("size of long long int: %ld byte(s)\n", sizeof(lli));
-printf("size of float: %ld byte(s)\n", sizeof(f));
+printf("size of a char: %ld byte(s)\n", sizeof(c));
+printf("size of an int: %ld byte(s)\n", sizeof(i));
+printf("size of a long int: %ld byte(s)\n", sizeof(li));
+printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+printf("size of a float: %ld byte(s)\n", sizeof(f));
 return (0);
 }
