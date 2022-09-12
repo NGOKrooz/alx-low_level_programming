08dfee90035adb23f4dd1a46bcd402b87f1bbf90
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-12 07:37:49 -0700

    yayaya

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,8 +19,7 @@ if (n > 0)
         printf("%d is postive\n", n);
 else if(n < 0)
         printf("%d is negative\n", n);
-else if(n == 0){
+else if(n == 0)
         printf("%d is zero\n", n);
-}
 return (0);
 }
