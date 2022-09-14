976ceae057d98e0a8ab6bd6f78e0cb1e0615c244
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-14 14:00:26 -0700

    x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,18 @@
-#include "main.h"
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+  #include "main.h"
 /**
  * print_alphabet --> prints the lower case 10 times
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 int j;
 int a;
 for (j = 'a'; j <= 'z'; j++)
-int a = j * 10;
+a = j * 10;
 {
 _putchar(a);
 }
@@ -15,3 +20,6 @@ _putchar('\n');
 
 }
 
+
+    return 0;
+}
