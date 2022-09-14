60b14d7b32bd2464993fec9daaf1de6bd4005aec
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-14 14:47:26 -0700

    great

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet_x10 --> prints the lower case 10 times
+ * print_alphabet --> prints the lower case 10 times
  */
-void print_alphabet_x10(void)
+void print_alphabet(void);
 {
 int j;
-int a;
 
-for (a = 0; a < 10; a++)
-{
 for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
 _putchar('\n');
-}
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,18 @@
 #include "main.h"
 /**
- * print_alphabet --> prints the lower case 10 times
+ * print_alphabet_x10 --> prints the lower case 10 times
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 int j;
 int a;
+
+for (a = 0; a < 10; a++)
+{
 for (j = 'a'; j <= 'z'; j++)
-a = j * 10;
 {
 _putchar(j);
 }
-
 _putchar('\n');
-
 }
-write("%d", a);
-
-    return 0;
 }
