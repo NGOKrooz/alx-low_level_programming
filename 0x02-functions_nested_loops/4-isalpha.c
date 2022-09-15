8aaf1afa48b6013c1012ca9b2bb6896e23687156
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-14 18:39:08 -0700

    baba

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,6 +2,7 @@
 /**
 * print_alphabet_x10 --> prints the alpha case 10 times
 * @c: is a character
+* description: This prints _islower
 * Return: returns 1 and 0 depending on condition
 */
 int int _isalpha(int c)
