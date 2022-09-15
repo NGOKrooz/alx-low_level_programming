18e19a0b595e417e09c18d6cd385f104d12dae94
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-14 18:36:50 -0700

    cool

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-* print_alphabet_x10 --> prints the lower case 10 times
+* print_alphabet_x10 --> prints the alpha case 10 times
 * @c: is a character
 * Return: returns 1 and 0 depending on condition
 */
-int _islower(int c)
+int int _isalpha(int c)
 {
 return (c >= 'A'&& c <= 'Z');
 }
