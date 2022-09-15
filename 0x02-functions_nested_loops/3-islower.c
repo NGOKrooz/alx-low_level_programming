93030560416b3a768859ab03f437b1b380565d9c
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-14 18:06:16 -0700

    fixed

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* print_alphabet_x10 --> prints the lower case 10 times
+* @c: is a character
+* Return: returns 1 and 0 depending on condition
+*/
+int _islower(int c);
+{
+return(c >= 'a'&& c <=z);
+}
