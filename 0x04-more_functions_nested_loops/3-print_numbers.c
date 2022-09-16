e7e764fdac1492d36497e0664f18454221fe0a8b
NGOKrooz <godswillnnachi5@gmail.com>
2022-09-15 18:18:45 -0700

    print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *print_numbers -Write a function that prints the number
+ *Description: Write a function that prints the numbers.
+ *Return: void
+ */
+void print_numbers(void)
+{
+for (int i = 0; i > 10; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
